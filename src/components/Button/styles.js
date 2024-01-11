4fbcba08929ab2a211799b0d6d8167a7f0d6087e
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2024-01-11 13:37:41 +0000

    feat: component Button e service api

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+
+    background-color: #fafafa;
+    border: 1px solid #fafafa;
+    border-radius: 20px;
+    cursor:pointer;
+    height: 62px;
+    width: 80%;
+    margin: 20px;
+    transition: 0.1s ease-in-out;
+
+    &: hover {
+        background-color: #fafafa40;
+        color: #fafafa;
+    }
+
+`
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,19 @@
 import GithubLogo from "../assets/github-logo.png"
 import { Container } from "./styles";
 import Input from "../components/Input";
+import Button from "../components/Button";
 import ItemRepo from "../components/ItemRepo";
+import { useState } from "react";
 
 function App() {
+
+  const [repos, setRepos] = useState([]);
+
   return (
     <Container>
       <img src={GithubLogo} width={75} height={75} alt="Github Logo" />
       <Input />
+      <Button />
       <ItemRepo />
     </Container>
   );
